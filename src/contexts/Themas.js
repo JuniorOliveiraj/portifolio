6e8799d28c0853e6f25416f981debe78b3ef7fbe
junior <junioroliveira.belem@gmail.com>
2025-08-23 12:02:20 -0300

    Refatoração: Remover código não utilizado e simplificar o contexto de temas

--- src/contexts/Themas.js
+++ src/contexts/Themas.js
@@ -1,39 +1,14 @@
-import { useState, createContext, useEffect, } from 'react';
-import axios from 'axios';
-import urlApi from '../_mock/url';
-// components
-
-// mock
-//import USERLIST from '../../_mock/user';
+import { useState, createContext, } from 'react';
 
 
-
-
-
-const url = urlApi;
 export const AlteracaoThema = createContext({});
 
 export const AlterThema = ({ children }) => {
-  const [ok, setOk] = useState(false);
 
   const [isLoading, setIsLoading] = useState(true);
   const [onFilterName, setOnFilterName] = useState("");
-  const [noticiasFavoritas, setNoticiasFavoritas] = useState([]);
   const [darkModeThem, setDarkModeThem] = useState(false);
 
-
-
-  // useEffect(() => {
-  //   axios.get('https://api-node-psi.vercel.app/users')
-  //   .then(response => {
-  //     console.log(response.data);
-  //     alert('foi')
-  //   })
-  //   .catch(error => {
-  //     console.error(error);
-  //     alert('n~~ao foi')
-  //   });
-  // }, []);
   const debounce = (func, delay) => {
     let timeoutId;
     return (...args) => {
@@ -45,74 +20,10 @@ export const AlterThema = ({ children }) => {
       }, delay);
     };
   };
- 
-
-
-  async function adicionarFavorito(id, noticia, status, news_id) {
-    setIsLoading(false);
-    const caminho = '/favoritos/adicionar';
-    debounce(() => {
-      axios.get(`${url}${caminho}`, {
-        params: {
-          id: id,
-          noticia: noticia,
-          status: status,
-          news_id: news_id
-        }
-      })
-        .then((response) => {
-          setIsLoading(false);
-
-        })
-        .catch((error) => {
-          console.error(error);
-
-        });
-    }, 1000)
-
-      ();
-  }
-
-
-  useEffect(() => {
-    async function listarFavorito(id) {
-      setIsLoading(false);
-      const caminho = '/favoritos/listar';
-      axios.get(`${url}${caminho}`, {
-        params: {
-          id: id,
-        }
-      })
-        .then((response) => {
-          setIsLoading(false);
-          setNoticiasFavoritas(response.data);
-          setOk(response.data.articles = !0 && true)
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-
-
-    }
-    function loadUserFromLocalStorage() {
-      const userString = localStorage.getItem('user');
-      if (userString) {
-        listarFavorito(JSON.parse(userString).uid);
-      }
-    }
-
-    loadUserFromLocalStorage();
-
-  }, []);
-
-
-
-  // Define a lista final de notícias, com o status atualizado
-
 
   return (
     <AlteracaoThema.Provider
-      value={{ darkModeThem, setDarkModeThem, isLoading, setIsLoading, ok, onFilterName, setOnFilterName, adicionarFavorito, noticiasFavoritas, debounce }}>
+      value={{ darkModeThem, setDarkModeThem, isLoading, setIsLoading, onFilterName, setOnFilterName, debounce }}>
       {children}
     </AlteracaoThema.Provider>
   )
